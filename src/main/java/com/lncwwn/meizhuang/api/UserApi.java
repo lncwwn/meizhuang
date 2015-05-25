14e1fdf341233568278c5951382d065f2d7feea4
victor <lncwwn@gmail.com>
2015-05-25 14:22:55 +0800

    refined

--- src/main/java/com/lncwwn/meizhuang/api/UserApi.java
+++ src/main/java/com/lncwwn/meizhuang/api/UserApi.java
@@ -1,11 +1,13 @@
 package com.lncwwn.meizhuang.api;
 
+import com.lncwwn.meizhuang.facade.impl.UserFacade;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Created with idea community version.
+ * define user api
  *
  * @author victor.li
  * @date 5/20/15
@@ -13,12 +15,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class UserApi {
 
+    @Autowired
+    private UserFacade userFacade;
+
     /**
      * user register api
      * @return
      */
     @RequestMapping(value = "user/register", method = RequestMethod.POST)
     public Object register(String params) {
+        //userFacade.register();
         return null;
     }
 
