6df726df35cbb9735526c1217f454df1b1a510a1
victor <lncwwn@gmail.com>
2015-05-29 00:38:05 +0800

    refined

---
+++ src/main/webapp/static/js/common.js
@@ -0,0 +1,16 @@
+/**
+ * common utils module
+ * @author victor.li
+ * @date 2015/05/29
+ */
+define(['jquery', 'bootstrap'], function(jquery, bootstrap) {
+
+    'use strict';
+
+    function showWarn(text) {
+
+    }
+
+    return {};
+
+});
