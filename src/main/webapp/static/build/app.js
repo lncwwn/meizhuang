b68fd109c0b5f5706ecefd2cca2de55aa701d540
victor <lncwwn@gmail.com>
2015-06-24 23:50:35 +0800

    refined

--- src/main/webapp/static/build/app.js
+++ src/main/webapp/static/build/app.js
@@ -35483,14 +35483,32 @@ webpackJsonp([0,1],[
 	var Link = ReactRouter.Link;
 
 	var Signup = React.createClass({displayName: "Signup",
+	    handleSubmit: function(e) {
+	        e.preventDefault();
+	        var nick = this.refs.nick.getDOMNode().value;
+	        var password = this.refs.password.getDOMNode().value;
+	        var passwordConfirm = this.refs.password_confirm.getDOMNode().value;
+	        var email = this.refs.email.getDOMNode().value;
+
+	        if (!nick || !password || !passwordConfirm || !email) {
+	            return;
+	        }
+	        if (password !== passwordConfirm) {
+	            return;
+	        }
+	        var params = {nick: nick, password: password, email: email};
+	        $.post(MZ.base + this.props.url, 'params=' + JSON.stringify(params), function(data) {
+	            console.log(data);
+	        });
+	    },
 	    render: function() {
 	        return (
-	            React.createElement("form", {className: "form-area"}, 
+	            React.createElement("form", {className: "form-area", onSubmit: this.handleSubmit}, 
 	                React.createElement("h3", null, "用户注册"), 
-	                React.createElement("input", {type: "text", className: "form-control", name: "nick", placeholder: "用户名", autofocus: "true"}), 
-	                React.createElement("input", {type: "password", className: "form-control", name: "password", placeholder: "密码"}), 
-	                React.createElement("input", {type: "password", className: "form-control", name: "password_confirm", placeholder: "再次输入密码"}), 
-	                React.createElement("input", {type: "text", className: "form-control", name: "email", placeholder: "邮箱帐号"}), 
+	                React.createElement("input", {type: "text", className: "form-control", ref: "nick", name: "nick", placeholder: "用户名", autofocus: "true"}), 
+	                React.createElement("input", {type: "password", className: "form-control", ref: "password", name: "password", placeholder: "密码"}), 
+	                React.createElement("input", {type: "password", className: "form-control", ref: "password_confirm", name: "password_confirm", placeholder: "再次输入密码"}), 
+	                React.createElement("input", {type: "text", className: "form-control", ref: "email", name: "email", placeholder: "邮箱帐号"}), 
 	                React.createElement("button", {className: "btn btn-default btn-primary btn-block js-user-register-btn"}, "注册"), 
 	                React.createElement("div", {className: "form-bottom"}, 
 	                    "已有账户? ", React.createElement(Link, {to: "login"}, "立即登录")
--- src/main/webapp/static/js/views/signup.js
+++ src/main/webapp/static/js/views/signup.js
@@ -10,14 +10,32 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 
 var Signup = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var nick = this.refs.nick.getDOMNode().value;
+        var password = this.refs.password.getDOMNode().value;
+        var passwordConfirm = this.refs.password_confirm.getDOMNode().value;
+        var email = this.refs.email.getDOMNode().value;
+
+        if (!nick || !password || !passwordConfirm || !email) {
+            return;
+        }
+        if (password !== passwordConfirm) {
+            return;
+        }
+        var params = {nick: nick, password: password, email: email};
+        $.post(MZ.base + this.props.url, 'params=' + JSON.stringify(params), function(data) {
+            console.log(data);
+        });
+    },
     render: function() {
         return (
-            <form className='form-area'>
+            <form className='form-area' onSubmit={this.handleSubmit}>
                 <h3>用户注册</h3>
-                <input type='text' className='form-control' name='nick' placeholder='用户名' autofocus='true'/>
-                <input type='password' className='form-control' name='password' placeholder='密码'/>
-                <input type='password' className='form-control' name='password_confirm' placeholder='再次输入密码'/>
-                <input type='text' className='form-control' name='email' placeholder='邮箱帐号'/>
+                <input type='text' className='form-control' ref='nick' name='nick' placeholder='用户名' autofocus='true'/>
+                <input type='password' className='form-control' ref='password' name='password' placeholder='密码'/>
+                <input type='password' className='form-control' ref='password_confirm' name='password_confirm' placeholder='再次输入密码'/>
+                <input type='text' className='form-control' ref='email' name='email' placeholder='邮箱帐号'/>
                 <button className='btn btn-default btn-primary btn-block js-user-register-btn'>注册</button>
                 <div className='form-bottom'>
                     已有账户? <Link to='login'>立即登录</Link>
