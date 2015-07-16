23355b65275cd289b50639a87b6c9653e2523dd3
victor <lncwwn@gmail.com>
2015-07-16 23:36:49 +0800

    refined

---
+++ mz-api/src/main/java/com/lncwwn/meizhuang/api/BasicApi.java
@@ -0,0 +1,44 @@
+package com.lncwwn.meizhuang.api;
+
+import com.alibaba.fastjson.JSON;
+import com.lncwwn.meizhuang.basic.pojo.User;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Api基类
+ *
+ * @author victor.li
+ * @date 5/20/15
+ */
+public class BasicApi {
+
+    protected User getLoginUser(String nick) {
+        User user = (User) RequestContextHolder.getRequestAttributes()
+                .getAttribute(nick, RequestAttributes.SCOPE_GLOBAL_SESSION);
+        return user;
+    }
+
+    protected void setLoginUser(String nick, User user) {
+        RequestContextHolder.getRequestAttributes()
+                .setAttribute(nick, user, RequestAttributes.SCOPE_GLOBAL_SESSION);
+    }
+
+    protected void removeLoginUser(String nick) {
+        RequestContextHolder.getRequestAttributes()
+                .removeAttribute(nick, RequestAttributes.SCOPE_GLOBAL_SESSION);
+    }
+
+    protected String handler(int code, String msg, Object data) {
+        Map<String, Object> obj = new HashMap<>();
+        obj.put("code", code);
+        obj.put("msg", msg);
+        obj.put("data", data);
+
+        return JSON.toJSONString(obj);
+    }
+
+}
---
+++ mz-api/src/main/java/com/lncwwn/meizhuang/api/CategoryApi.java
@@ -0,0 +1,33 @@
+package com.lncwwn.meizhuang.api;
+
+import com.lncwwn.meizhuang.basic.pojo.Category;
+import com.lncwwn.meizhuang.core.service.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * Created with idea community version.
+ *
+ * @author victor.li
+ * @date 7/8/15
+ */
+@Controller
+@RequestMapping("/category")
+public class CategoryApi extends BasicApi {
+
+    @Autowired
+    private ICategoryService categoryService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET, produces = "text/html;charset=UTF-8")
+    @ResponseBody
+    public String categories() {
+        List<Category> categories = categoryService.categories();
+        return handler(1, "success", categories);
+    }
+
+}
